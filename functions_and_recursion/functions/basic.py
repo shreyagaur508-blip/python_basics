76025de9781fc432784b57d634b1aad70a3efa65
Shreya Gaur <shreyagaur508@gmail.com>
2026-08-03 20:59:41 +0530

    commit

---
+++ functions_and_recursion/functions/basic.py
@@ -0,0 +1,14 @@
+#function definition
+def sum(a,b): #parameters
+    sum = a+b
+    print(sum)
+    return sum
+
+sum(10,20) #function call with arguments
+
+
+#function with no parameters and return value
+def hello():
+    print("Hello World")
+
+hello()
