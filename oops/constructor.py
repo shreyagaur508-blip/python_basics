8ec712747850cb8d22d33e008b6a143c63b74067
Shreya Gaur <shreyagaur508@gmail.com>
2026-08-06 19:32:51 +0530

    commit

---
+++ oops/constructor.py
@@ -0,0 +1,7 @@
+class Student:
+    name = "Shreya"
+    def __init__(self):
+        print(self)
+        print("Adding new data in Database")
+
+s1 = Student()
---
+++ oops/oops.py
@@ -0,0 +1,7 @@
+#creating class
+class Student:
+    name = "Shreya"
+
+#object
+s1 = Student()
+print(s1.name)
